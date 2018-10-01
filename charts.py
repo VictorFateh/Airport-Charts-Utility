9d9cdc7f9412af0fa7a70eed257dbb7152dd075a
Victor F <victorfateh@yahoo.com>
2018-09-30 17:50:39 -0700

    flightaware.com change

--- charts.py
+++ charts.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import urllib.request
 
 import requests
@@ -8,39 +7,44 @@ from bs4 import BeautifulSoup
 
 def start_download(input_list):
     for airport in input_list:
-        for i in scrape_page(airport):
-            download_file(airport, i[0], i[1])
+        for i in scrape_flightaware(airport):
+            if download_file(airport, i[0], i[1], i[2]):
+                print("{} - {} downloaded".format(airport, i[1]))
 
 
-def download_file(airport, name, url):
-    if not os.path.exists("Charts/{}".format(airport)):
-        os.makedirs("Charts/{}".format(airport))
-    location = "Charts/{}/{}.pdf".format(airport, name.replace('/', ''))
-    urllib.request.urlretrieve(url, location)
-
-
-# Given an airport ICAO, returns list
-# [("APPROACH NAME", "PDF URL"), ("...", "..."), ...]
-def scrape_page(airport):
+def download_file(airport, diagram_type, name, url):
+    location = "Charts/{}/{}/{}.pdf".format(airport, diagram_type, name.replace('/', ''))
     try:
-        data = requests.get("https://www.airnav.com/airport/{}".format(airport))
+        if not os.path.exists("Charts/{}/{}".format(airport, diagram_type)):
+            os.makedirs("Charts/{}/{}".format(airport, diagram_type))
+        urllib.request.urlretrieve(url, location)
+        return True
+    except Exception as e:
+        print("Unable to download {}".format(airport), e)
+        os.rmdir("Charts/{}".format(airport))
+        return False
+
+
+# Diagram: https://flightaware.com/resources/airport/SFO/APD/AIRPORT+DIAGRAM/pdf
+# Procedures: https://flightaware.com/resources/airport/KSFO/procedures
+# Returns list ready to download [TYPE, NAME, PDF_URL]
+def scrape_flightaware(airport):
+    try:
+        data = requests.get("https://flightaware.com/resources/airport/{}/procedures".format(airport))
 
         soup = BeautifulSoup(data.text, 'html.parser')
 
         pdf_list = []
 
-        pdf_table = soup.find(text="Instrument Procedures").parent.findNext('table')
+        data_row = soup.find_all('div', {'class': 'row'})[2]
 
-        for link in pdf_table.find_all('a'):
-            title_pdf_tuple = (link.findPrevious('td').findPrevious('td').findPrevious('td').text, link['href'][8:])
-            pdf_list.append(title_pdf_tuple)
+        for table in data_row.find_all('table'):
+            for link in table.find_all('a'):
+                download_info = (link.parent.findPrevious('td').getText(), link.get_text(),
+                                 "https://flightaware.com/resources/{}/pdf".format(link['href'][11:]))
+                pdf_list.append(download_info)
 
         return pdf_list
 
     except Exception as e:
         return "Error: ", e
-
-
-if __name__ == "__main__":
-    test_list = ['KSJC', 'KSFO', 'KPAO']
-    start_download(test_list)
